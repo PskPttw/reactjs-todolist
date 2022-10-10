101d8225e02bd40b22dbbbf1ba77755835f262ed
MinjiPark <pun.p0107@gmail.com>
2022-10-11 00:26:24 +0700

    Setup client side.

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import styles from "./style";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+      <h1>Body</h1>
+    </div>
+  );
+}
+
+export default App;
